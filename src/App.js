4cb1d473e7873006f1b4f3e45701aa544e30c2ad
sahand <sahandasg56@gmail.com>
2024-09-18 10:48:14 +0330

    install som package and develop demo of project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import fakeData from "./fakeData";
 function App() {
   return (
-    <div>
-
+    <div className="w-100 min-vh-100 bg-black d-flex justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded d-flex flex-column justify-content-center align-items-start p-3 ">
+          <h1>Questions And Answers About Login</h1>
+          <div className="border border-2 rounded mt-md-5 w-75">
+              <div className="d-flex align-items-center justify-content-between gap-5 p-2">
+              <h3>Question</h3>
+              <button className="border-0 p-2 px-3 rounded">+</button>
+              </div>
+              {/*<p>Answer</p>*/}
+          </div>
+      </div>
     </div>
   );
 }
